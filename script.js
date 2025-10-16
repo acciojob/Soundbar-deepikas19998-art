b0055a74ec56d2c2c1d38cc9c586ef860559e1dd
deepikas19998-art <deepikas19998@gmail.com>
2025-10-16 13:00:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+// List of sounds
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+// Create Audio objects
+const audioElements = {};
+sounds.forEach(sound => {
+  audioElements[sound] = new Audio(`sounds/${sound}.mp3`);
+});
+
+// Select all buttons
+const buttons = document.querySelectorAll('.btn');
+
+// Play and stop logic
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const soundName = btn.innerText;
+
+    if (soundName === 'stop') {
+      stopSounds();
+    } else {
+      stopSounds();
+      audioElements[soundName].play();
+    }
+  });
+});
+
+// Stop all sounds
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = audioElements[sound];
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
