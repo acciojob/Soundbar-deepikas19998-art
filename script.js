7a050ff33032ff84c94a83eec10c6a67de0547a3
deepikas19998-art <deepikas19998@gmail.com>
2025-10-16 13:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,70 +1,29 @@
-const baseUrl = 'http://localhost:3000';  // Adjust this to your actual base URL
+const applause = new Audio('sounds/applause.mp3');
+const boo = new Audio('sounds/boo.mp3');
+const gasp = new Audio('sounds/gasp.mp3');
+const tada = new Audio('sounds/tada.mp3');
+const victory = new Audio('sounds/victory.mp3');
+const wrong = new Audio('sounds/wrong.mp3');
 
-// Helper to assert audio or video is playing
-function expectPlayingAudio() {
-  // Get audio or video element(s)
-  cy.get('audio,video').should('exist').then($media => {
-    // Check if any media element is playing
-    const isPlaying = [...$media].some(media => {
-      return !media.paused && !media.ended && media.currentTime > 0;
-    });
+document.getElementById('applause').addEventListener('click', () => applause.play());
+document.getElementById('boo').addEventListener('click', () => boo.play());
+document.getElementById('gasp').addEventListener('click', () => gasp.play());
+document.getElementById('tada').addEventListener('click', () => tada.play());
+document.getElementById('victory').addEventListener('click', () => victory.play());
+document.getElementById('wrong').addEventListener('click', () => wrong.play());
 
-    // Assert at least one media element is playing
-    expect(isPlaying).to.be.true;
-  });
-}
-
-describe('Media playback tests', () => {
-  beforeEach(() => {
-    cy.visit(baseUrl + '/main.html');
-  });
-
-  it('Click first button plays media', () => {
-    cy.get('.btn').first().click();
-
-    // Wait up to 10s for media element to appear
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
-
-  it('Click second button plays media', () => {
-    cy.get('.btn').eq(1).click();
-
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
-
-  it('Click third button plays media', () => {
-    cy.get('.btn').eq(2).click();
-
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
-
-  it('Click fourth button plays media', () => {
-    cy.get('.btn').eq(3).click();
-
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
-
-  it('Click fifth button plays media', () => {
-    cy.get('.btn').eq(4).click();
-
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
-
-  it('Click sixth button plays media', () => {
-    cy.get('.btn').eq(5).click();
-
-    cy.get('audio,video', { timeout: 10000 }).should('exist');
-
-    expectPlayingAudio();
-  });
+document.getElementById('stop').addEventListener('click', () => {
+  applause.pause();
+  boo.pause();
+  gasp.pause();
+  tada.pause();
+  victory.pause();
+  wrong.pause();
+  // Resetting the playback position to the start of each sound file.
+  applause.currentTime = 0;
+  boo.currentTime = 0;
+  gasp.currentTime = 0;
+  tada.currentTime = 0;
+  victory.currentTime = 0;
+  wrong.currentTime = 0;
 });
